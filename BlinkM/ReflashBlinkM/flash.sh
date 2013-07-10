fa8b6bfe0588357fcb8b3fc8c3decdd98b37ea8b
Jérôme Labidurie <jerome@labidurie.fr>
2013-07-11 00:56:34 +0200

    correct fuse to be modified !

--- BlinkM/ReflashBlinkM/flash.sh
+++ BlinkM/ReflashBlinkM/flash.sh
@@ -16,5 +16,5 @@ PGM="avrisp -b 19200"
 avrdude -V -p t45 -c $PGM  -P $USB -U lfuse:r:-:b -U hfuse:r:-:b -U efuse:r:-:b
 # flash program & eeprom
 avrdude -V -p t45 -c $PGM -P $USB -U flash:w:blinkmv1.hex:a -U eeprom:w:blinkmv1.eep
-# write lfuse
-avrdude -V -p t45 -c $PGM -P $USB -U lfuse:w:0xDD:m
+# write hfuse
+avrdude -V -p t45 -c $PGM -P $USB -U hfuse:w:0xDD:m
